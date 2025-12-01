59e5be763b9a78d0c5612684b0ac7bb52cf85b30
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2025-12-01 09:23:49 +0100

    feat(base) : add layouts and pages

---
+++ app/pages/blog/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>va voir mes articles</h1>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/pages/test.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+ definePageMeta({
+     layout: 'aside'
+ })
+</script>
+
+<template>
+    <div>
+        <h1>test</h1>
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
