3f3729793cdaa02d90f14909ab57713b640b07a5
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2025-12-01 14:33:18 +0100

    feat(components) : add test components page

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,19 +1,39 @@
 <script lang="ts">
- definePageMeta({
-     layout: 'aside'
- })
 
- export default {}
 </script>
 
 <template>
-    <div>
-        <h1>test</h1>
+    <div class="ct-buttons">
+        <!-- Variante Default -->
+        <section class="ct-buttons__default">
+            <MyButton size="tiny">Tiny</MyButton>
+            <MyButton size="small">Small</MyButton>
+            <MyButton size="medium">Medium</MyButton>
+            <MyButton size="large">Large</MyButton>
+            <MyButton size="giant" disabled>Giant (Disabled)</MyButton>
+        </section>
+
+        <!-- Variante Outline -->
+        <section class="ct-buttons__outline">
+            <MyButton variant="outline" size="tiny">Tiny</MyButton>
+            <MyButton variant="outline" size="small">Small</MyButton>
+            <MyButton variant="outline" size="medium">Medium</MyButton>
+            <MyButton variant="outline" size="large">Large</MyButton>
+            <MyButton variant="outline" size="giant" disabled>Giant (Disabled)</MyButton>
+        </section>
     </div>
 </template>
 
 
-
-<style scoped>
-
+<style lang="scss" scoped>
+.ct-buttons {
+  &__default,
+  &__outline {
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 2rem;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
