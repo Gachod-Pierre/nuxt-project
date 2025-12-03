a94eeec513adced85502d77be05f7139d9982ff9
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2025-12-03 14:35:17 +0100

    feat(header) : add styles and logic to header

--- app/components/MyHeader.vue
+++ app/components/MyHeader.vue
@@ -1,4 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
 
+const isOpen = ref(false)
+
+function toggleMenu () {
+  isOpen.value = !isOpen.value
+}
+</script>
 
 <template>
   <header class="header">
@@ -38,3 +46,140 @@
   </header>
 </template>
 
+<style scoped lang="scss">
+
+.header {
+  background: rgb(244, 244, 244);
+  color: white;
+  width: 100%;
+  position: relative;
+  z-index: 50;
+  border-bottom: 5px solid rgba(0, 0, 0, 0.1);
+
+  &__container {
+    max-width: 1280px;
+    margin: auto;
+    padding: 1rem 1.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+
+  &__logo img {
+    width: 50px;
+    height: auto;
+    display: block;
+  }
+
+
+  &__nav {
+    display: flex;
+    gap: 2rem;
+
+    @include medium-down {
+    display: none;
+  }
+  }
+
+  &__link {
+    color: black;
+    font-weight: 500;
+    text-decoration: none;
+    transition: opacity 0.2s;
+    
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+
+
+  &__actions {
+    display: flex;
+    align-items: center;
+    gap: 1.2rem;
+
+    @include medium-down {
+    display: none;
+  }
+  }
+
+  &__user img {
+    width: 26px;
+    height: 26px;
+    cursor: pointer;
+  }
+
+  &__burger {
+    display: none;
+    background: none;
+    border: none;
+    padding: 0.3rem;
+    cursor: pointer;
+
+    @include medium-down {
+      display: flex;
+      flex-direction: column;
+      gap: 6px;
+    }
+
+    span {
+      width: 28px;
+      height: 3px;
+      background: white;
+      border-radius: 5px;
+      transition: 0.3s ease;
+
+      &.is-open:nth-child(1) {
+        transform: translateY(9px) rotate(45deg);
+      }
+      &.is-open:nth-child(2) {
+        opacity: 0;
+      }
+      &.is-open:nth-child(3) {
+        transform: translateY(-9px) rotate(-45deg);
+      }
+    }
+  }
+
+  &__mobile-menu {
+    display: flex;
+    flex-direction: column;
+    background: #111;
+    padding: 1rem 1.5rem;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    gap: 1rem;
+    transform: translateY(-100%);
+    opacity: 0;
+    pointer-events: none;
+    transition:
+      opacity 0.3s ease,
+      transform 0.3s ease;
+
+    &.is-open {
+      transform: translateY(0);
+      opacity: 1;
+      pointer-events: auto;
+    }
+  }
+
+  &__mobile-link {
+    color: white;
+    text-decoration: none;
+    font-size: 1.1rem;
+    padding: 0.8rem 0;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+}
+</style>
