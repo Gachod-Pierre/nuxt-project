600557c640f0c7fc25519f6b07458229c31b1ef5
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2025-12-10 11:21:08 +0100

    feat(sanity) : using Sanity Singleton datas for the hero

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,39 @@
 <script setup lang="ts">
+import type { SanityHomePage } from '~/types/cms/homePage'
+
+const HOME_PAGE_QUERY = groq`*[_type == "homePage"][0]{
+  title,
+  subtitle,
+  seoTitle,
+  description,
+  heroBanner
+}`
+
+const { data: homePage } = await useLazySanityQuery<SanityHomePage>(HOME_PAGE_QUERY)
 </script>
 
 <template>
-  <div>
-    <MyTitle :level="1" size="xl">HomePage</MyTitle>
+  <div v-if="homePage">
+    <!-- Hero Banner -->
+    <MyHero :image="homePage.heroBanner" :alt="homePage.title" />
+
+    <!-- Content -->
+    <div class="homepage">
+      <MyTitle :level="1" size="xl">{{ homePage.title }}</MyTitle>
+      <p v-if="homePage.subtitle" class="homepage__subtitle">
+        {{ homePage.subtitle }}
+      </p>
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.homepage {
+  padding: 2rem;
+
+  &__subtitle {
+    margin-top: 1rem;
+    font-size: 1.25rem;
+  }
+}
+</style>
