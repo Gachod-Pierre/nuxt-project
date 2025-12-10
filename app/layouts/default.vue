1ed70896d8d042c33033d6b1dc35afed711bab8d
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2025-12-10 11:20:30 +0100

    feat(sanity) : using sanity singleton datas for the header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+import type { SanitySiteSettings } from '~/types/cms/siteSettings'
+
+// request for Sanity siteSettings
+
+const query = groq`*[_type == "siteSettings"][0]`
+
+const { data } = await useLazySanityQuery<SanitySiteSettings>(query)
+
+// ... champs seo remplis depuis Sanity
+</script>
+
 <template>
   <div class="layout-default">
-    <my-header />
+    <my-header v-if="data" v-bind="{ navigation: data.navigation, logo: data.logo }"/>
     <main class="layout-default__main">
       <slot />
     </main>
